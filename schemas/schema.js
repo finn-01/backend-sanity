984008128d874e503371da84c4b0911a588602c1
finn-01 <nguyensam576@gmail.com>
2022-02-19 13:45:51 +0700

    commit

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,36 +1,32 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import createSchema from "part:@sanity/base/schema-creator";
 
-// We import object and document schemas
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+// Then import schema types from any plugins that might expose them
+import schemaTypes from "all:part:@sanity/base/schema-type";
+import works from "./works";
+import testimonials from "./testimonials";
+import brands from "./brands";
+import abouts from "./abouts";
+import experiences from "./experiences";
+import skills from "./skills";
+import workExperience from "./workExperience";
+import contact from "./contact";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([
-    // The following are document types which will appear
-    // in the studio.
-    movie,
-    person,
-    screening,
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
-    blockContent,
-    plotSummary,
-    plotSummaries,
-    castMember,
-    crewMember,
-  ]),
-})
+	// We name our schema
+	name: "default",
+	// Then proceed to concatenate our document type
+	// to the ones provided by any plugins that are installed
+	types: schemaTypes.concat([
+		works,
+		testimonials,
+		brands,
+		abouts,
+		skills,
+		workExperience,
+		experiences,
+		contact,
+		/* Your types here! */
+	]),
+});
